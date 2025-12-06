7d6cde0cbbeae36d8b93019e339a4cc1dbd530a7
sitaratoon <ajay393798@gmail.com>
2025-12-06 16:39:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003455925059"))
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
-IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
+IS_FSUB = os.environ.get("IS_FSUB", "True").lower() == "true"  # Set "True" For Enable Force Subscribe
 AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002355394644 -1003022855971").split())) # Add Multiple channel ids
 
 EMOJIS = ["❤‍🔥", "🤗", "🔥", "🥰", "👏", "😁", "😘", "🥶", "🤩", "👍", "😎", "🙏", "😍", "💯"]
